0975e453bacff292e5649a207667275057af1d28
aa0925779822-svg <aa0925779822@gmail.com>
2026-03-10 11:50:35 +0800

    week3_php

---
+++ test1.py
@@ -0,0 +1,23 @@
+def square(y):
+	print(f"{y} 的三次方是 {y**3}")
+
+
+
+
+
+
+
+
+print("python程式測試")
+x = int(input("請輸入一個整數"))
+#x +=10
+
+if (x<=0):
+	print(f"您輸入的值是{X}，小於等於0")
+else:
+	print("您輸入的值是{X}，大於0")
+	for i in range(x,x+1):
+		#print(i，end="；")
+		square(i)
+
+
